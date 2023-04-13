c16fa1e327280a894f414321bc6054bbe858ffd7
anthonymBD <97461227+anthonymBD@users.noreply.github.com>
2023-04-13 12:48:42 -0500

    update scroll to work without
    the error in Scrolled if not there is now fixed

--- scroller.js
+++ scroller.js
@@ -1,28 +1,52 @@
+
+
 let tl = gsap.timeline();
-tl.fromTo(
-  ".desktop-h1",
-  { opacity: 0, y: -200 },
-  { y: 0, opacity: 1, duration: 1.4 }
-).fromTo(
-  ".img-fluid",
-  {
-    opacity: 0,
-    x: -300,
-  },
-  {
-    opacity: 1,
-    x: 0,
-    ease: "sine.in",
-  },
-  ">-1"
-);
 
-let st = ScrollTrigger.create({
-  trigger: ".withoutscroll",
-  pin: ".withoutscroll",
-  scrub: true,
-  start: "top",
+let hero = document.querySelector(".desktop-h1")
+let sideScroller = document.querySelector(".withoutscroll")
 
-  endTrigger: ".square",
-  end: "center bottom",
-});
+
+
+
+if (hero) {
+    tl.fromTo(
+        ".desktop-h1",
+        { opacity: 0, y: -200 },
+        { y: 0, opacity: 1, duration: 1.4 }
+    ).fromTo(
+        ".img-fluid",
+        {
+            opacity: 0,
+            x: -300,
+        },
+        {
+            opacity: 1,
+            x: 0,
+            ease: "sine.in",
+        },
+        ">-1"
+    );
+}
+
+if (sideScroller) {
+
+    let st = ScrollTrigger.create({
+        trigger: ".withoutscroll",
+        pin: ".withoutscroll",
+        scrub: true,
+        start: "300px center",
+        endTrigger: ".square",
+        end: "-250px 200px ",
+    });
+
+    // tl.fromTo(
+    //   ".layers",
+    //  { opacity: 0, y: -200 },
+
+    //   { y: 0, opacity: 1, duration: 1.4, stagger: 0.5 }
+    //)
+}
+
+
+
+
